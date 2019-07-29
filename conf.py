f92fe439b6da2c369439024bf8c33d468d188594
RobiFag <robifagandini@hotmail.it>
2019-07-29 16:02:27 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -97,7 +97,7 @@ html_theme_options = {
     'canonical_url': '',
     #'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
     'logo_only': False,
-	'html_logo': img/4ISTITUZIONALI.png,
+	#'html_logo': img/4ISTITUZIONALI.png,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
